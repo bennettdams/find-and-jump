563b2604cf23cda8c08f93bb96f0b7426d4676c2
Bennett Dams <dams.bennett@gmail.com>
2023-08-26 15:38:14 +0200

    Implement executing searches

--- src/extension.ts
+++ src/extension.ts
@@ -16,6 +16,30 @@ function setStatusBarMessage(msg: string) {
   statusBar.text = msg;
 }
 
+const searchDecorationType = vscode.window.createTextEditorDecorationType({
+  // TODO Pick custom color
+  backgroundColor: { id: 'myExtension.searchHighlight' },
+});
+
+function showTooltipMessage(
+  msg: string,
+  type: 'default' | 'error' = 'default',
+) {
+  switch (type) {
+    case 'default': {
+      vscode.window.showInformationMessage(msg);
+      break;
+    }
+    case 'error': {
+      vscode.window.showErrorMessage(msg);
+      break;
+    }
+    default: {
+      throw new Error(`Unkown tooltip type: ${type}`);
+    }
+  }
+}
+
 function setSearchModeStatus(isActiveNew: boolean) {
   isSearchModeActive = isActiveNew;
   vscode.commands.executeCommand(
@@ -56,7 +80,7 @@ export function activate(context: vscode.ExtensionContext) {
     (event) => {
       if (isSearchModeActive) {
         searchInput += event.text;
-        // perform search
+        executeSearch(searchInput);
       } else {
         // fall back to the default type command
         vscode.commands.executeCommand('default:type', event);
@@ -77,6 +101,79 @@ export function activate(context: vscode.ExtensionContext) {
     disposableCommandType,
     disposableCommandExitSearchMode,
   );
+}
+
+function executeSearch(searchTerm: string) {
+  console.debug('######## Executing search for: ', searchTerm);
+
+  const activeTextEditor = vscode.window.activeTextEditor;
+  if (!activeTextEditor) {
+    showTooltipMessage('No active text editor.', 'error');
+    return;
+  }
+
+  const document = activeTextEditor.document;
+  const documentText = document.getText();
+  const regex = new RegExp(searchTerm, 'g');
+
+  setSearchModeStatus(true);
+
+  const matches: Matches = Array.from(
+    documentText.matchAll(regex),
+    (match) => match.index,
+  );
+  const noOfMatches = matches.length;
+
+  if (noOfMatches === 0) {
+    showTooltipMessage(`No matches found for ${searchTerm}.`);
+    return;
+  } else {
+    setStatusBarMessage(
+      `Searching for: '${searchTerm}' | ${noOfMatches} result${
+        noOfMatches === 1 ? '' : 's | Press TAB to cycle through'
+      }`,
+    );
+  }
+
+  function createRange(matchIndex: number | undefined, searchTerm: string) {
+    if (typeof matchIndex === 'undefined') {
+      showTooltipMessage(
+        'Something went wrong. See logs for more information. Error: Missing match index',
+      );
+      throw new Error('Missing match index');
+    }
+
+    const startPos = document.positionAt(matchIndex);
+    const endPos = document.positionAt(matchIndex + searchTerm.length);
+    const range = new vscode.Range(startPos, endPos);
+
+    return range;
+  }
+
+  // Apply decorations to the matches
+  let matchDecorations: vscode.DecorationOptions[] = matches
+    .filter((matchIndex) => typeof matchIndex === 'number')
+    .map((matchIndex) => {
+      return { range: createRange(matchIndex, searchTerm) };
+    });
+
+  // Initially set the selection to the first match
+  const firstMatchDecoration = matchDecorations.at(0);
+  if (!!firstMatchDecoration) {
+    activeTextEditor.selection = new vscode.Selection(
+      firstMatchDecoration.range.start,
+      firstMatchDecoration.range.end,
+    );
+
+    activeTextEditor.setDecorations(searchDecorationType, matchDecorations);
+  }
+
+  // Store the matches and search term in a context for navigation
+  searchContext = { searchTerm, matches, currentIndex: 0 };
+
+  // TODO Handle TAB to cycle
+
+  console.debug('######## End of executing search');
 }
 
 function exitSearchMode() {
