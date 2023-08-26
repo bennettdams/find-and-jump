b38c85e0250c05eac6d526d6d9abb2cd6a1fdf6c
Bennett Dams <dams.bennett@gmail.com>
2023-08-26 15:12:43 +0200

    Initialize status bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,29 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "find-and-jump" is now active!');
+let statusBar: vscode.StatusBarItem;
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    'find-and-jump.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from Find &amp; Jump!');
-    },
-  );
-
-  context.subscriptions.push(disposable);
+function setStatusBarMessage(msg: string) {
+  statusBar.text = msg;
 }
 
-// This method is called when your extension is deactivated
+function initializeStatusBar() {
+  statusBar = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    100,
+  );
+  statusBar.command = 'find-and-jump.exitSearchMode';
+  statusBar.tooltip = 'Click to exit search mode.';
+  statusBar.show();
+
+  setStatusBarMessage('Extension activated');
+}
+
+/** Executed on activation. */
+export function activate(context: vscode.ExtensionContext) {
+  console.debug('Activated');
+
+  initializeStatusBar();
+}
+
+/** Executed on deactivation. */
 export function deactivate() {}
